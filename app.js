1d261c57864cc8e4e89eb3f9e9a38107bfe5cf95
Wilson <p780729@hotmail.com>
2020-03-29 06:04:11 +0800

    add mysql database

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
+
 const port = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -33,7 +37,11 @@ app.get('/users/register', (req, res) => {
 })
 // 註冊檢查
 app.post('/users/register', (req, res) => {
-  res.send('register')
+  User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  }).then(user => res.redirect('/'))
 })
 // 登出
 app.get('/users/logout', (req, res) => {
