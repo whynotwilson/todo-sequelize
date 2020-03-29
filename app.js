16bc396eee83d755dfa683cc53f3f310123c5ad7
Wilson <p780729@hotmail.com>
2020-03-29 16:35:08 +0800

    Add home and user routers

--- app.js
+++ app.js
@@ -17,36 +17,9 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-// 設定路由
-// 首頁
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
-// 認證系統的路由
-// 登入頁面
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-// 登入檢查
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
-// 註冊頁面
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-// 註冊檢查
-app.post('/users/register', (req, res) => {
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  }).then(user => res.redirect('/'))
-})
-// 登出
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
+// 使用路由器
+app.use('/', require('./routes/home'))
+app.use('/users', require('./routes/user'))
 
 app.listen(port, () => {
   console.log('app is running......')
