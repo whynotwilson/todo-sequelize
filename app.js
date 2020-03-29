a1aea7e2d2e102aee3c4af7731b07d456afe90f2
Wilson <p780729@hotmail.com>
2020-03-29 20:48:41 +0800

    Add facebook  auth feature

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.use(session({
   saveUninitialized: 'false'
 }))
 
+if (process.env.NODE_ENV !== 'production') { // 如果不是 production 模式
+  require('dotenv').config() // 使用 dotenv 讀取 .env 檔案
+}
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
@@ -46,6 +50,7 @@ app.use((req, res, next) => {
 app.use('/', require('./routes/home'))
 app.use('/users', require('./routes/user'))
 app.use('/todos', require('./routes/todo'))
+app.use('/auth', require('./routes/auths'))
 
 app.listen(port, () => {
   console.log('app is running......')
--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,5 @@
 const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
 const bcrypt = require('bcryptjs')
 
 // 載入 User model
@@ -27,9 +28,47 @@ module.exports = passport => {
     })
   )
 
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['email', 'displayName']
+  }, (accessToken, refreshToken, profile, done) => {
+    User.findOne({ where: { email: profile._json.email } })
+      .then(user => {
+        if (!user) {
+          var randomPassword = Math.random().toString(36).slice(-8)
+          bcrypt.genSalt(10, (err, salt) => {
+            if (err) throw err
+
+            bcrypt.hash(randomPassword, salt, (err, hash) => {
+              if (err) console.error(err)
+
+              var newUser = new User({
+                name: profile._json.name,
+                email: profile._json.email,
+                password: hash
+              })
+
+              newUser.save().then(user => {
+                return done(null, user)
+              }).catch(err => {
+                console.log(err)
+              })
+            })
+          })
+        } else {
+          return done(null, user)
+        }
+      })
+  }))
+
+  // 序列化，只存 id 就好，節省資料量
   passport.serializeUser((user, done) => {
     done(null, user.id)
   })
+
+  // 取出 user 資料以後，可能傳給前端樣板，故加入.lean().exec()
   passport.deserializeUser((id, done) => {
     User.findByPk(id).then((user) => {
       user = user.get()
