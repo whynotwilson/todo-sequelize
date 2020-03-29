79e8709472698db9c2ab41c314eb7d2f712c42f6
Wilson <p780729@hotmail.com>
2020-03-29 16:53:55 +0800

    add login using passport and express-session

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 const express = require('express')
 const app = express()
+const port = 3000
 
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
+const session = require('express-session')
+const passport = require('passport')
+
 const db = require('./models')
 const Todo = db.Todo
 const User = db.User
 
-const port = 3000
+app.use(session({
+  secret: 'todo-sequelize',
+  resave: 'false',
+  saveUninitialized: 'false'
+}))
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+// 設定 passport & session
+app.use(passport.initialize())
+app.use(passport.session())
+require('./config/passport')(passport)
+app.use((req, res, next) => {
+  res.locals = req.user
+  next()
+})
 
 // 使用路由器
 app.use('/', require('./routes/home'))
